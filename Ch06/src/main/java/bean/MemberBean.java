0a394c65a18b0fa2f2606aaa3a068476bf3fc649
a01074882716 <a01074882716@gmail.com>
2022-10-12 13:52:37 +0900

    2022/10/12

---
+++ Ch06/src/main/java/bean/MemberBean.java
@@ -0,0 +1,51 @@
+package bean;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+public class MemberBean {
+	private String uid;
+	private String name;
+	private String hp;
+	private String pos;
+	private int dep;
+	private String rDate;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getPos() {
+		return pos;
+	}
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+	public int getDep() {
+		return dep;
+	}
+	public void setDep(int dep) {
+		this.dep = dep;
+	}
+	public String getrDate() {
+		return rDate;
+	}
+	public void setrDate(String rDate) {
+		this.rDate = rDate;
+	}
+	
+	
+	
+}
